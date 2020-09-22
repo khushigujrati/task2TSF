d9f7ae6a7e484e26de3d84a3164c73efa64c2250
Khushi Gujrati <gujratikhushi1312@gmail.com>
2020-09-22 19:25:02 +0530

    Final Commit

--- backend.js
+++ backend.js
@@ -17,129 +17,138 @@ function uploadData() {
   var info = database.ref("creditInfo");
   var infoUser = {
     credit: 100,
-    email: "suvidhi@gmail.com",
-    name: "Suvidhi",
-    uid: 10,
+    email: "aatiqShafiq@gmail.com",
+    name: "Aatiq Shafiq",
+    uid: 11,
   };
   info.push(infoUser);
 }
 
-// uploadData();
+//uploadData();
 // Table Creation dynamically
-function LoadData() {
-  const table = document.getElementById("tableBody");
-  var realData = firebase.database();
-  var ref = realData.ref("creditInfo");
-  ref.on("value", gotData, errData);
+var txStatus = 0;
+var senderEmail, recEmail, amount;
+var detailsGlobal, keyGlobal, ksGlobal;
 
-  function gotData(data) {
-    table.innerHTML = "";
-    var details = data.val();
-    var keys = Object.keys(details);
-    for (var i = 0; i < keys.length; i++) {
-      var k = keys[i];
-      var tr = document.createElement("tr");
-      var id = document.createElement("td");
-      var names = document.createElement("td");
-      id.innerHTML = details[k].uid;
-      names.innerHTML = details[k].name;
-      names.id = details[k].uid;
-      tr.appendChild(id);
-      tr.appendChild(names);
-      var transferButton = document.createElement("button");
-      transferButton.className = "btn btn-danger";
-      transferButton.innerHTML = "Check Info";
-      transferButton.id = details[k].uid;
-      tr.appendChild(transferButton);
-      table.appendChild(tr);
-    }
-    document.addEventListener("click", function (e) {
-      var emailDiv = document.getElementById("email-row");
-      var creditDiv = document.getElementById("credit-row");
-
-      for (var i = 0; i < keys.length; i++) {
-        var k = keys[i];
-        if (e.target.id == details[k].uid) {
-          var name = details[k].name;
-          var email = details[k].email;
-          var credit = details[k].credit;
-          emailDiv.innerHTML = "<p>Email :" + email + "</p>";
-          creditDiv.innerHTML = "<p>Credit :" + credit + "</p>";
-
-          $("#userModal").modal("show");
-          $("#transferBtn").click(function () {
-            var senderList = document.createElement("option");
-            var sender = document.getElementById("sender");
-            sender.innerHTML = "";
-            senderList.innerHTML = email;
-            sender.appendChild(senderList);
-            var rec = document.getElementById("receiver");
-            rec.innerHTML = "";
-            for (var i = 0; i < keys.length; i++) {
-              var ks = keys[i];
-              var recList = document.createElement("option");
-
-              if (email == details[ks].email) continue;
-
-              recList.innerHTML = details[ks].email;
-
-              rec.appendChild(recList);
-            }
-            $("#infoModal").modal("show");
-            var sendBtn = document.getElementById("sendBtn");
-            sendBtn.addEventListener("click", (e) => {
-              var sender = document.getElementById("sender");
-              var rec = document.getElementById("receiver");
-              var money = document.getElementById("amount");
-              if (money.value <= 0) {
-                window.alert("Please enter a valid amount to transfer!");
-              } else if (details[ks].credit < money.value) {
-                window.alert("Insufficient Funds in account!");
-              } else if (money.value > 0 && details[ks].credit >= money.value) {
-                transaction(
-                  sender.value,
-                  rec.value,
-                  money.value,
-                  details,
-                  keys
-                );
-              }
-            });
-          });
-        }
-      }
-    });
+const table = document.getElementById("tableBody");
+var realData = firebase.database();
+var ref = realData.ref("creditInfo");
+ref.on("value", gotData, errData);
+function gotData(data) {
+  table.innerHTML = "";
+  var details = data.val();
+  detailsGlobal = details;
+  var keys = Object.keys(details);
+  keyGlobal = keys;
+  for (var i = 0; i < keys.length; i++) {
+    var k = keys[i];
+    var tr = document.createElement("tr");
+    var id = document.createElement("td");
+    var names = document.createElement("td");
+    id.innerHTML = details[k].uid;
+    names.innerHTML = details[k].name;
+    names.id = details[k].uid;
+    tr.appendChild(id);
+    tr.appendChild(names);
+    var transferButton = document.createElement("button");
+    transferButton.className = "btn btn-danger";
+    transferButton.innerHTML = "Check Info";
+    transferButton.id = details[k].uid;
+    tr.appendChild(transferButton);
+    table.appendChild(tr);
   }
 }
+
 function errData(err) {
   console.log("Error!");
   console.log(err);
 }
+
+//
+document.addEventListener("click", function (e) {
+  var emailDiv = document.getElementById("email-row");
+  var creditDiv = document.getElementById("credit-row");
+
+  for (var i = 0; i < keyGlobal.length; i++) {
+    var k = keyGlobal[i];
+    if (e.target.id == detailsGlobal[k].uid) {
+      var email = detailsGlobal[k].email;
+      var credit = detailsGlobal[k].credit;
+      emailDiv.innerHTML = "<p>Email :" + email + "</p>";
+      creditDiv.innerHTML = "<p>Credit :" + credit + "</p>";
+      senderEmail = email;
+      amount = credit;
+      $("#userModal").modal("show");
+    }
+  }
+});
+//
+function transerInit() {
+  var senderList = document.createElement("option");
+  var sender = document.getElementById("sender");
+  sender.innerHTML = "";
+  senderList.innerHTML = senderEmail;
+  sender.appendChild(senderList);
+  var rec = document.getElementById("receiver");
+  rec.innerHTML = "";
+  for (var i = 0; i < keyGlobal.length; i++) {
+    var ks = keyGlobal[i];
+    var recList = document.createElement("option");
+    if (senderEmail == detailsGlobal[ks].email) continue;
+    recList.innerHTML = detailsGlobal[ks].email;
+    rec.appendChild(recList);
+  }
+
+  $("#infoModal").modal("show");
+}
+//
+function sendMoney() {
+  var sender = document.getElementById("sender");
+  var rec = document.getElementById("receiver");
+  var money = document.getElementById("amount");
+  var sk, rk;
+  for (var i = 0; i < keyGlobal.length; i++) {
+    var k = keyGlobal[i];
+    if (detailsGlobal[k].email == sender.value) {
+      sk = k;
+      console.log(sk);
+    }
+  }
+  for (var i = 0; i < keyGlobal.length; i++) {
+    var k = keyGlobal[i];
+    if (detailsGlobal[k].email == rec.value) {
+      rk = k;
+      console.log(rk);
+    }
+  }
+  if (money.value <= 0) {
+    window.alert("Please enter a valid amount to transfer!");
+    window.location.href = "http://127.0.0.1:5500/";
+  } else if (parseFloat(detailsGlobal[sk].credit) < parseFloat(money.value)) {
+    console.log(parseFloat(detailsGlobal[sk].credit), money.value);
+    window.alert("Insufficient Funds in account!");
+    window.location.href = "http://127.0.0.1:5500/";
+  } else if (
+    parseFloat(money.value) > 0 &&
+    parseFloat(detailsGlobal[sk].credit) >= money.value
+  ) {
+    console.log(detailsGlobal[sk].credit, money.value);
+    transaction(sender.value, rec.value, money.value, sk, rk);
+  }
+}
+//
 // Transaction
-function transaction(sender, rec, money, details, keys) {
+function transaction(sender, rec, money, senderKey, recKey) {
   let userRef = firebase.database().ref("creditInfo");
 
-  if (keys != null) {
-    for (var i = 0; i < keys.length; i++) {
-      var ks = keys[i];
+  if (senderKey != null || recKey != null) {
+    userRef.child(senderKey).update({
+      credit: parseFloat(detailsGlobal[senderKey].credit) - parseFloat(money),
+    });
+    userRef.child(recKey).update({
+      credit: parseFloat(detailsGlobal[recKey].credit) + parseFloat(money),
+    });
 
-      var SenderkeyInfo = "";
-      var RecKeyInfo = "";
-      if (sender == details[ks].email) {
-        SenderkeyInfo = keys[i];
-
-        userRef.child(SenderkeyInfo).update({
-          credit: parseFloat(details[ks].credit) - parseFloat(money),
-        });
-      }
-      if (rec == details[ks].email) {
-        RecKeyInf = keys[i];
-
-        userRef.child(RecKeyInf).update({
-          credit: parseFloat(details[ks].credit) + parseFloat(money),
-        });
-      }
-    }
     userRef = firebase.database().ref("TransactionDetails");
     var transData = {
       date: Date(),
@@ -149,6 +158,7 @@ function transaction(sender, rec, money, details, keys) {
     };
     userRef.push(transData);
     window.alert("Transaction Successful!");
+    window.location.href = "https://khushigujrati.000webhostapp.com/";
   }
 }
 
